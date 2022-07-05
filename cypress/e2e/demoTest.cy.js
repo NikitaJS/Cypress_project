43b460e1da56990ce25568e5c9a22f1938e0fc34
Nikita <87061404+NikitaJS@users.noreply.github.com>
2022-07-05 21:35:50 +0200

    add comment findComandContains

--- cypress/e2e/demoTest.cy.js
+++ cypress/e2e/demoTest.cy.js
@@ -1,6 +1,6 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://uuusoftware.com/'),
-    cy.viewport(1200, 800)
+    cy.viewport(1920, 1040)
   })
 })
--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -2,66 +2,31 @@
 /// <reference types="Cypress" />
 
 
-// it('By id', () => {
-//     cy.visit("https://uuusoftware.com/contact-us")
-//     cy.get('#email')
-// });
-
-// it('By Class', () => {
-//     cy.visit("https://uuusoftware.com/contact-us")
-//     cy.get('.form-group')
-// })
-
-// it.only('By Tag', () => {
-//     cy.visit("https://uuusoftware.com/")
-//     cy.get('nav')
-// })
-
-// it('By Tag value', () => {
-//     cy.visit("https://uuusoftware.com/contact-us")
-//     cy.get('[name="email"]')
-// })
-
-// it('By Tag value', () => {
-//     cy.visit("https://uuusoftware.com/contact-us")
-//     cy.get('[type="submit"]')
-// });
-
-// it('By Diffent Types', () => {
-//     cy.visit("https://uuusoftware.com/contact-us")
-//     cy.get('button[type="submit"]')
-// });
-
-// 30 строка это тоже самое что и 39
-// Комбинирующий способ 
-
-// it('By Diffent Types', () => {
-//     cy.visit("https://uuusoftware.com/contact-us")
-//     cy.get('button[type="submit"][class="submit-btn"]')
-// });
+    it('', () => {
+        cy.visit('https://uuusoftware.com/'),
+            cy.viewport(1920, 1040)
+                cy.contains('Contact us');
+    })
 
 
-// Поиск по уникальному классу или если зашифрован class пример: hfjsdfsdhkj  
-// it('By Contains name', () => {
-//     cy.visit("https://uuusoftware.com/")
-//     cy.get('[class^="three-column-grid"]')
-// });
+    it('', () => {
+    cy.visit('https://uuusoftware.com/'),
+        cy.viewport(1920, 1040)
+            cy.contains('div', 'contact us', {matchCase: false});
+    // чтобы игнорировать регистр нужно
+})
 
+it.only('', () => {
+    cy.visit('https://uuusoftware.com/'),
+        cy.viewport(1920, 1040)
+    cy.get('footer').contains('About Us');
+})
 
-// Формула предназначена для работы с индексами 
-// it.only('Using Get with Find and Eq', () => {
-//     cy.visit("https://next.privat24.ua/deposit/open")
-//     cy.get('tbody').find('td').find('div').find('button').eq(3)
-// });
-
-// Comment 
-it('Using Get with Find and Eq', () => {
-    cy.viewport(1800, 700)
-    cy.visit("https://uuusoftware.com/")
-    cy.get('header').find('div').find('a').eq(0)
-});
-
-
+it('', () => {
+    cy.visit('https://uuusoftware.com/'),
+        cy.viewport(1920, 1040)
+    cy.get('footer').contains('About Us');
+})
 
 
 
