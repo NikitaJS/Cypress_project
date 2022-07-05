530c6dcd973178e905493bbbbc08f25e1b30ffe2
Nikita <87061404+NikitaJS@users.noreply.github.com>
2022-07-05 19:20:19 +0200

    lector about finding elements by Get, Find, Eq

---
+++ cypress/e2e/demoTest.cy.js
@@ -0,0 +1,6 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://uuusoftware.com/'),
+    cy.viewport(1200, 800)
+  })
+})
---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,69 @@
+
+/// <reference types="Cypress" />
+
+
+// it('By id', () => {
+//     cy.visit("https://uuusoftware.com/contact-us")
+//     cy.get('#email')
+// });
+
+// it('By Class', () => {
+//     cy.visit("https://uuusoftware.com/contact-us")
+//     cy.get('.form-group')
+// })
+
+// it.only('By Tag', () => {
+//     cy.visit("https://uuusoftware.com/")
+//     cy.get('nav')
+// })
+
+// it('By Tag value', () => {
+//     cy.visit("https://uuusoftware.com/contact-us")
+//     cy.get('[name="email"]')
+// })
+
+// it('By Tag value', () => {
+//     cy.visit("https://uuusoftware.com/contact-us")
+//     cy.get('[type="submit"]')
+// });
+
+// it('By Diffent Types', () => {
+//     cy.visit("https://uuusoftware.com/contact-us")
+//     cy.get('button[type="submit"]')
+// });
+
+// 30 строка это тоже самое что и 39
+// Комбинирующий способ 
+
+// it('By Diffent Types', () => {
+//     cy.visit("https://uuusoftware.com/contact-us")
+//     cy.get('button[type="submit"][class="submit-btn"]')
+// });
+
+
+// Поиск по уникальному классу или если зашифрован class пример: hfjsdfsdhkj  
+// it('By Contains name', () => {
+//     cy.visit("https://uuusoftware.com/")
+//     cy.get('[class^="three-column-grid"]')
+// });
+
+
+// Формула предназначена для работы с индексами 
+// it.only('Using Get with Find and Eq', () => {
+//     cy.visit("https://next.privat24.ua/deposit/open")
+//     cy.get('tbody').find('td').find('div').find('button').eq(3)
+// });
+
+it('Using Get with Find and Eq', () => {
+    cy.viewport(1800, 700)
+    cy.visit("https://uuusoftware.com/")
+    cy.get('header').find('div').find('a').eq(0)
+});
+
+
+
+
+
+
+
+
