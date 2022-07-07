af03c2dab5ea14f8bf1a0a20bc931e0608b70f1c
Nikita <87061404+NikitaJS@users.noreply.github.com>
2022-07-07 23:41:35 +0200

    lecturer about should and expect

--- cypress/e2e/demoTest.cy.js
+++ cypress/e2e/demoTest.cy.js
@@ -2,5 +2,9 @@ describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://uuusoftware.com/'),
     cy.viewport(1920, 1040)
+
   })
 })
+
+
+
--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -2,31 +2,47 @@
 /// <reference types="Cypress" />
 
 
-    it('', () => {
-        cy.visit('https://uuusoftware.com/'),
-            cy.viewport(1920, 1040)
-                cy.contains('Contact us');
+it('SHOULD', () => {
+    cy.visit('https://uuusoftware.com/contact-us')
+    cy.get('[name="first_name"]')
+        .type(100)
+        .should('have.value', 100)
+        .and('be.visible')
+})
+
+it('EXPECT', () => {
+    cy.visit('https://uuusoftware.com/contact-us')
+    cy.get('[name="first_name"]')
+    .type("Nikita").then( input => {
+        expect(input).to.have.value("Nikita")
+    })   
     })
 
 
-    it('', () => {
-    cy.visit('https://uuusoftware.com/'),
-        cy.viewport(1920, 1040)
-            cy.contains('div', 'contact us', {matchCase: false});
-    // чтобы игнорировать регистр нужно
+
+it('EXPECT', () => {
+    cy.visit('https://uuusoftware.com/contact-us')
+    cy.get('[value="less than $50k"]')
 })
 
-it.only('', () => {
-    cy.visit('https://uuusoftware.com/'),
-        cy.viewport(1920, 1040)
-    cy.get('footer').contains('About Us');
-})
+it('check and click burger', () => {
+    cy.visit('https://uuusoftware.com/contact-us')
+    cy.get('[class="burger"]').click()
+    .trigger('mouseov') 
+    })   
 
-it('', () => {
-    cy.visit('https://uuusoftware.com/'),
-        cy.viewport(1920, 1040)
-    cy.get('footer').contains('About Us');
-})
+
+    it('check is correct attr in button', () => {
+        cy.visit('https://uuusoftware.com/contact-us')
+        cy.contains('Send')
+        .should('have.attr', 'type')
+        })  
+//
+        it.only('check is correct URL', () => {
+            cy.visit('https://uuusoftware.com/')
+           cy.get('[href="https://goo.gl/maps/SEWV19wnQsh4DWms5"]')
+            cy.url().should('eq', 'https://goo.gl/maps/SEWV19wnQsh4DWms5')
+            })  
 
 
 
